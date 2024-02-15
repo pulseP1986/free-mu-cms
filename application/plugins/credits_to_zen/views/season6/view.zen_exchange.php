97550d3f3ec4b9c12c1e415379467d2da47c5093
pulseP1986 <160108743+pulseP1986@users.noreply.github.com>
2024-02-15 17:51:17 +0200

    Added credits to zen plugin

--- application/plugins/credits_to_zen/views/season6/view.zen_exchange.php
+++ application/plugins/credits_to_zen/views/season6/view.zen_exchange.php
@@ -32,28 +32,41 @@
                                         <?php endif;
                                         ?>
                                             <script>
-                                                var creditsExchange = new creditsExchange();
-                                                creditsExchange.setUrl('<?php echo $this->config->base_url . $this->request->get_controller();?>');
+                                                var zenExchange = new zenExchange();
+                                                zenExchange.setUrl('<?php echo $this->config->base_url . $this->request->get_controller();?>');
                                                 $(document).ready(function () {
-                                                    $('#credits_exchange_form').on("submit", function (e) {
+                                                    $('#zen_exchange_form').on("submit", function (e) {
                                                         e.preventDefault();
-                                                        creditsExchange.submit($(this));
+                                                        zenExchange.submit($(this));
                                                     });
                                                 });
                                             </script>
 
                                         <?php
-                                            list($cred, $cred2) = explode('/', $plugin_config['ratio']);
-                                            $cred2_trans = ($plugin_config['exchange_type'] == 1) ? $this->website->translate_credits(2, $this->session->userdata(['user' => 'server'])) : $this->website->translate_credits(1, $this->session->userdata(['user' => 'server']));
+                                            if(!empty($char_list)):
+                                            list($cred, $zen) = explode('/', $plugin_config['ratio']);
                                         ?>
                                             <div class="i_note"><?php echo __('Exchange info'); ?>:
                                                 <?php
-                                                    echo __('You can get') . ' ' . $cred2 . ' ' . $cred2_trans . ' for ' . $cred . ' ' . $this->website->translate_credits($plugin_config['exchange_type'], $this->session->userdata(['user' => 'server']));
+                                                    echo __('You can get') . ' ' . $this->website->zen_format($zen) . ' ' . __('Zen') . ' for ' . $cred . ' ' . $this->website->translate_credits($plugin_config['exchange_type'], $this->session->userdata(['user' => 'server']));
                                                 ?>
                                             </div>
                                             <div class="form">
-                                                <form method="post" action="" id="credits_exchange_form">
+                                                <form method="post" action="" id="zen_exchange_form">
                                                     <table>
+                                                        <tr>
+                                                            <td style="width: 150px;"><?php echo __('Location'); ?></td>
+                                                            <td>
+                                                                <select name="character">
+                                                                    <?php foreach($char_list as $chars): ?>
+                                                                        <option
+                                                                                value="<?php echo $chars['id']; ?>"><?php echo $chars['Name']; ?></option>
+                                                                    <?php endforeach; ?>
+                                                                    <option value="-1"><?php echo __('Warehouse'); ?></option>
+                                                                    <option value="-2"><?php echo __('Zen Wallet'); ?></option>
+                                                                </select>
+                                                            </td>
+                                                        </tr>
                                                         <tr>
                                                             <td style="width:150px;"><?php echo __('Amount of') . ' ' . $this->website->translate_credits($plugin_config['exchange_type'], $this->session->userdata(['user' => 'server'])); ?>
                                                                 :
@@ -61,23 +74,23 @@
                                                             <td><input type="text" id="game_currency" name="game_currency"
                                                                        value=""
                                                                        class="text"
-                                                                       onblur="creditsExchange.calculateCurrency($('#game_currency').val(), '<?php echo $plugin_config['ratio']; ?>');"
-                                                                       onkeyup="creditsExchange.calculateCurrency($('#game_currency').val(), '<?php echo $plugin_config['ratio']; ?>');"/>
+                                                                       onblur="zenExchange.calculateCurrency($('#game_currency').val(), '<?php echo $plugin_config['ratio']; ?>');"
+                                                                       onkeyup="zenExchange.calculateCurrency($('#game_currency').val(), '<?php echo $plugin_config['ratio']; ?>');"/>
                                                             </td>
                                                         </tr>
                                                         <tr>
-                                                            <td><?php echo __('Will Receive'); ?> <?php echo $cred2_trans; ?>
+                                                            <td><?php echo __('Will Receive'); ?> <?php echo __('Zen'); ?>
                                                                 :
                                                             </td>
-                                                            <td><input type="text" id="cred2"
-                                                                       name="cred2" value="" class="text"
+                                                            <td><input type="text" id="zen"
+                                                                       name="zen" value="" class="text"
                                                                        disabled/></td>
                                                         </tr>
                                                         <tr>
                                                             <td></td>
                                                             <td>
-                                                                <button type="submit" id="exchange_credits"
-                                                                        name="exchange_credits" disabled="disabled"
+                                                                <button type="submit" id="exchange_zen"
+                                                                        name="exchange_zen" disabled="disabled"
                                                                         class="button-style"><?php echo __('Submit'); ?></button>
                                                             </td>
                                                         </tr>
@@ -85,6 +98,9 @@
                                                 </form>
                                             </div>
                                         <?php
+                                        else:
+                                            echo '<div class="i_note">' . __('No characters found.') . '</div>';
+                                        endif;
                                         endif;
                                     endif;
                                 ?>
---
+++ assets/plugins/js/credits_to_zen.js
@@ -0,0 +1,75 @@
+function zenExchange() {
+    this.url = '';
+    this.zen = 0;
+    this.cred = 0;
+    this.total = 0;
+}
+
+zenExchange.prototype.setUrl = function (data) {
+    this.url = data;
+}
+zenExchange.prototype.saveSettings = function (form) {
+	
+    $.ajax({
+        type: "post",
+        dataType: "json",
+        url: this.url + "/save_settings",
+        data: form.serialize(),
+        success: function (data) {
+            if (data.error) {
+                noty($.parseJSON('{"text":"' + data.error + '","layout":"topRight","type":"error"}'));
+            }
+            else {
+				
+                noty($.parseJSON('{"text":"' + data.success + '","layout":"topRight","type":"success"}'));
+            }
+        }
+    });
+}
+zenExchange.prototype.calculateCurrency = function (val, ratio) {
+	this.cred = ratio.split("/")[0];
+    this.zen = ratio.split("/")[1];
+    if ((val.toString().search(/^-?[0-9]+$/) != 0)) {
+        $('#game_currency').val('0');
+        $('#zen').val('0');
+        if (typeof $('#exchange_zen').attr("disabled") == 'undefined' || $('#exchange_zen').attr("disabled") == false) {
+            $('#exchange_zen').attr("disabled", "disabled");
+        }
+    }
+    else {
+        if (val > 0) {
+            this.total = Math.floor(val / this.cred) * this.zen;
+            $('#zen').val(Math.floor(this.total));
+            if (($('#zen').val() != 0)) {
+                $('#exchange_zen').removeAttr("disabled");
+            }
+        }
+        else {
+            $('#zen').val(0);
+            $('#exchange_zen').attr("disabled", "disabled");
+        }
+    }
+}
+zenExchange.prototype.submit = function (form) {
+    $.ajax({
+        type: "post",
+        dataType: "json",
+        url: this.url + "/change_credits",
+        data: form.serialize(),
+        success: function (data) {
+            if (data.error) {
+                App.notice(App.lc.translate('Error').fetch(), 'error', data.error);
+            }
+            else {
+                App.notice(App.lc.translate('Success').fetch(), 'success', data.success);
+            }
+        }
+    });
+}
+
+
+
+
+
+
+
